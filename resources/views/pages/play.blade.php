26102894f03cd615c2773cfe75b951e84cc1e3ea
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-03-17 00:28:30 +0300

    Improved User Interface

--- resources/views/pages/play.blade.php
+++ resources/views/pages/play.blade.php
@@ -26,9 +26,9 @@
 		</div>
 		<hr/>
 		<div class="row">
-			<div class="col-md-3"><h3>Highest: <span id="high_score">{{$user->user_high_score}}</span></h3></div>
-			<div class="col-md-3"><h3>Missed: <span id="missed_score">0</span></h3></div>
-			<div class="col-md-3"><h3>Total: <span id="total_score">0</span></h3></div>
+			<div class="col-3 col-md-3 col-sm-3 col-xs-3"><h5>Highest: <span id="high_score">{{$user->user_high_score}}</span></h5></div>
+			<div class="col-3 col-md-3 col-sm-3 col-xs-3"><h5>Missed: <span id="missed_score">0</span></h5></div>
+			<div class="col-3 col-md-3 col-sm-3 col-xs-3"><h5>Total: <span id="total_score">0</span></h5></div>
 		</div>
 		
 		<hr/>
