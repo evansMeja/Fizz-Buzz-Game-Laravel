c096a5298a54e1f48829a97aa7102ded3d16e021
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-03-30 01:09:51 +0300

    added new game

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.base')
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8">EEEEE
             <div class="card">
                 <div class="jumbotron">Please Sign Up to Continue</div>
 
